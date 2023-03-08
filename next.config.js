a9bcd15ff5388743fafd7ce8d0333eb5395637d7
HigorR456 <higor.ruan456@gmail.com>
2023-03-08 16:20:57 -0300

    Change import syntax

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: false,
   devIndicators: {
         buildActivity: false,
-    }
+    },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-import '../styles/navigation.scss';
-import '../styles/pokemon.scss';
-import '../styles/cart.scss';
-import '../styles/title.scss';
-import '../styles/navBar.scss';
-import '../styles/myCart.scss';
-import '../styles/indexHome.scss';
-import '../styles/type.scss';
-import '../styles/item.scss';
-import '../styles/allPokemon.scss';
+import '@/styles/navigation.scss';
+import '@/styles/pokemon.scss';
+import '@/styles/cart.scss';
+import '@/styles/title.scss';
+import '@/styles/navBar.scss';
+import '@/styles/myCart.scss';
+import '@/styles/indexHome.scss';
+import '@/styles/type.scss';
+import '@/styles/item.scss';
+import '@/styles/allPokemon.scss';
 
 import { store } from '../app/store';
 import { Provider } from 'react-redux';
